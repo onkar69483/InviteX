a763769cfa8feaa925851d05af44923ee3c11da4
onkar69483 <onkar69483@gmail.com>
2024-12-14 22:46:32 +0600

    Frontend initial setup

---
+++ frontend/src/app/layout.js
@@ -0,0 +1,29 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "InviteX",
+  description: "InviteX is a web-based application designed to streamline event invitation and entry management for organizations. It eliminates the need for physical invitation cards, reducing costs and enhancing efficiency with a fully digital solution",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ frontend/src/app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-black">
+      <h1 className="text-white text-4xl font-extrabold text-center shadow-lg px-8 py-4">
+        Welcome to InviteX
+      </h1>
+    </div>
+  );
+}
